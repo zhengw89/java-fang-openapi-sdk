c01e944b7c93fb1678d593165941d8a49270d553
王征 <wangzheng@fang.com>
2017-08-28 17:21:02 +0800

    补充部分单元测试

--- src/test/java/com/fang/agent/openapi/sdk/test/SdkTest.java
+++ src/test/java/com/fang/agent/openapi/sdk/test/SdkTest.java
@@ -4,6 +4,7 @@ import static org.junit.Assert.*;
 
 import java.io.IOException;
 import java.util.ArrayList;
+import java.util.Date;
 import java.util.List;
 
 import org.junit.Ignore;
@@ -13,7 +14,9 @@ import com.fang.agent.openapi.Sdk;
 import com.fang.agent.openapi.bean.request.house.HouseDeleteRequest;
 import com.fang.agent.openapi.bean.request.house.HouseDetailRequest;
 import com.fang.agent.openapi.bean.request.house.HouseListRequest;
+import com.fang.agent.openapi.bean.request.house.HousePhotoAddRequest;
 import com.fang.agent.openapi.bean.request.house.HousePhotosRequest;
+import com.fang.agent.openapi.bean.request.house.RefreshSolutionListRequest;
 import com.fang.agent.openapi.bean.request.house.SaleHouseInputRequest;
 import com.fang.agent.openapi.bean.request.house.SaleHouseInputRequestBuilder;
 import com.fang.agent.openapi.bean.response.agent.AgentDetailResponse;
@@ -21,29 +24,43 @@ import com.fang.agent.openapi.bean.response.house.HouseDeleteResponse;
 import com.fang.agent.openapi.bean.response.house.HouseDetailResponse;
 import com.fang.agent.openapi.bean.response.house.HouseInputResponse;
 import com.fang.agent.openapi.bean.response.house.HouseListResponse;
+import com.fang.agent.openapi.bean.response.house.HousePhotoAddReponse;
 import com.fang.agent.openapi.bean.response.house.HousePhotosResponse;
+import com.fang.agent.openapi.bean.response.house.RefreshSolutionListResponse;
 import com.fang.agent.openapi.constants.OpenApiConstants;
+import com.fang.agent.openapi.util.SecurityUtil;
 
 public class SdkTest {
 
-	private static final String HOST = "h";
-	private static final int COMPANY_ID = 1;
-	private static final String API_KEY = "ak";
-	private static final int AGENT_ID = 1;
-	private static final String USERNAME = "un";
-	private static final String PASSWORD = "pw";
+	private static final String HOST = OpenApiConstants.SH_HOTS;
+	private static final int COMPANY_ID = 11;
+	private static final String API_KEY = "11";
+	private static final int AGENT_ID = -1;
+	private static final String USERNAME = "11";
+	private static final String PASSWORD = "11";
 	private static final Sdk SDK = new Sdk(HOST, String.valueOf(COMPANY_ID), API_KEY, true);
+
+	@Ignore
+	@Test
+	public void SigTest() {
+
+		String sig = SecurityUtil.getSignature(AGENT_ID, API_KEY);
+		System.out.println(sig);
+		assertTrue(true);
+	}
 
 	@Test
 	@Ignore
 	public void AuthorizeTest() {
 		try {
 			String response = SDK.AgentAuthentication(USERNAME, PASSWORD);
-			System.out.println(response);
+			System.out.println(String.format("%s - %s", String.valueOf(new Date()), response));
 			assertNotNull(response);
+
 		} catch (Exception e) {
 			e.printStackTrace();
 		}
+
 	}
 
 	@Test
@@ -59,9 +76,12 @@ public class SdkTest {
 		request.setKitchen(1);
 		request.setLiveArea(111.1);
 		request.setPrice(500);
-		request.setProjName("裕隆新村");
+		request.setProjName("富力现代");
+		request.setDistrict("区县");
+		request.setComarea("商圈");
+		request.setAddress("地址");
 		request.setRoom(1);
-		request.setTitle("TTTTTTTT");
+		request.setTitle("测试标题");
 		request.setToilet(1);
 		request.setPayInfo("个人产权");
 		request.setBalcony(1);
@@ -89,7 +109,7 @@ public class SdkTest {
 	public void HouseListTest() {
 		HouseListRequest request = new HouseListRequest();
 		request.setCurPage(1);
-		request.setHouseStaut(OpenApiConstants.HOUSE_STATUS_PUBLISHED);
+		request.setHouseStaut(OpenApiConstants.HOUSE_STATUS_UNPUBLISHED);
 		request.setPageSize(10);
 		request.setHouseType(OpenApiConstants.HOUSE_TYPE_SALE);
 		HouseListResponse response = SDK.HouseList(AGENT_ID, request);
@@ -122,6 +142,23 @@ public class SdkTest {
 
 	@Test
 	@Ignore
+	public void HousePhotoAddTest() {
+		HousePhotoAddRequest request = new HousePhotoAddRequest();
+		request.setAlbumType(OpenApiConstants.HOUSE_PHOTO_ALBUM_TYPE_SN);
+		request.setHouseId(187511497);
+		request.setHouseStatus(OpenApiConstants.HOUSE_STATUS_PUBLISHED);
+		request.setPhotoName("室内图");
+		request.setPhotoUrl("http://cdnsfb.soufunimg.com/1/2017_6/15/M10/15/059673efe7a34e60939297beac124b62.jpg");
+		request.setPurposeType(OpenApiConstants.PURPOSE_TYPE_HOUSE);
+		request.setHouseType(OpenApiConstants.HOUSE_TYPE_SALE);
+
+		HousePhotoAddReponse reponse = SDK.HousePhotoAdd(AGENT_ID, request);
+
+		assertNotNull(reponse);
+	}
+
+	@Test
+	@Ignore
 	public void UploadFileTest() {
 		try {
 			String result = SDK.UploadImg(AGENT_ID, "C:\\Users\\Zheng\\Desktop\\ttt.jpg");
@@ -148,5 +185,15 @@ public class SdkTest {
 
 		assertTrue(request.getHouseType().equals(OpenApiConstants.HOUSE_TYPE_LEASE));
 		assertTrue(request.getRoom() == 10);
+	}
+
+	@Test
+	@Ignore
+	public void RefreshSolutionListTest() {
+		RefreshSolutionListResponse response = SDK.RefreshSolutionList(AGENT_ID, new RefreshSolutionListRequest());
+
+		assertNotNull(response);
+		assertTrue(response.getData().size() != 0);
+
 	}
 }
